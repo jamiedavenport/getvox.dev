5d60cba139f5f2a4a871b122d6448b7cda9ccb21
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-10-18 17:11:14 +0100

    Nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
-import react from '@vitejs/plugin-react';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
-import mdx from 'fumadocs-mdx/vite';
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "fumadocs-mdx/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    mdx(await import('./source.config')),
+    mdx(await import("./source.config")),
     tailwindcss(),
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tanstackStart({
       prerender: {
         enabled: true,
       },
     }),
+    nitroV2Plugin(),
     react(),
   ],
 });
